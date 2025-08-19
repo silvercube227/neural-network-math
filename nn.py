ac8d07381c2ecca49d8d8f273b32b25c3aca5583
Bennett Ye <bennettye@gmail.com>
2025-08-18 21:51:32 -0700

    added softmax and second dense/activaiton layer

--- nn.py
+++ nn.py
@@ -22,13 +22,26 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        #axis - only along rows (0) or columns (1) -using columns for neuron outputs
+        #exponentiate each value in inputs to get unnormalized probs, remove largest to prevent exploding nums
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        #normalize for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 dense1.forward(X)
-
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+dense1.forward(X)
 #take in ouput from previous layer for activation laye
 activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-print(activation1.output[:5])
+print(activation2.output[:5])
