e6fc2b81c67cf1639e80788c4dfb2e90019e460a
Bennett Ye <bennettye@gmail.com>
2025-08-18 18:33:27 -0700

    implemented ReLu

---
+++ nn.py
@@ -0,0 +1,34 @@
+#previous files were math notes, building here
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class Layer_Dense:
+    #initialize
+    def __init__(self, n_inputs, n_neurons):
+        #temporarily generate random weights
+        self.weights = 0.01  * np.random.randn(n_inputs, n_neurons) #inputs before neurons allows us to avoid transposing this matrix
+        #zero bias
+        self.biases = np.zeros((1,n_neurons))
+
+    #forward pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense1.forward(X)
+
+#take in ouput from previous layer for activation laye
+activation1.forward(dense1.output)
+
+print(activation1.output[:5])
