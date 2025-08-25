51793ef09fd22bdf68f2291959cee83dd1db0fe2
Bennett Ye <bennettye@gmail.com>
2025-08-24 21:03:42 -0700

    added categorical crossentropy

--- nn.py
+++ nn.py
@@ -31,6 +31,28 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+
+class Categorial_Crossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        #clip both sides to not drag mean toward any val
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        #probabilities for categorical values
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
@@ -38,10 +60,13 @@ activation1 = Activation_ReLU()
 dense1.forward(X)
 dense2 = Layer_Dense(3,3)
 activation2 = Activation_Softmax()
+loss_function = Categorial_Crossentropy()
+
 dense1.forward(X)
 #take in ouput from previous layer for activation laye
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
+loss = loss_function.calculate(activation2.output, y)
 
-print(activation2.output[:5])
+print("loss: " + loss)
